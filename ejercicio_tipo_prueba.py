b52d9e4f766cd37977cd1798c0c7318f6f7f752d
erick <er.rosales@duocuc.cl>
2024-06-14 13:52:05 -0400

    lista sin validar inputs

--- ejercicio_tipo_prueba.py
+++ ejercicio_tipo_prueba.py
@@ -1,5 +1,8 @@
 import os,time
 
+trabajadores=[]
+cargos=("CEO", "DESARROLADOR", "ANALISTA")#0:ceo 1:desarrollador 2:analista
+
 while True:
     os.system('cls')
     print("MENU TRABAJODERS")
@@ -11,7 +14,18 @@ while True:
     os.system('cls')
 
     if opc==1:
-        pass
+        print("Registro trabajadores")
+        nombre_apellido=input("ingrese nombre y apellido") 
+        cargo=int(input("Ingrese cargo (1.CEO 2.DESARROLLADOR 3. ANALISTA): "))
+        sueldo_bruto=int(input("INGRESE SUELDO BRUTO: ")) 
+        dcto_salud=int(sueldo_bruto-(sueldo_bruto*0.93) )
+        dcto_AFP=int(sueldo_bruto-(sueldo_bruto*0.93) )
+        sueldo_liquido=sueldo_bruto*0.81 
+        traba=[nombre_apellido,cargos[cargo-1],sueldo_bruto,dcto_salud,dcto_AFP,sueldo_liquido]
+        trabajadores.append(traba)
+        print("TRABAJADOR REGISTRADO CON ÉXITO") 
+
+
     elif opc==2:
         pass
     elif opc==3:

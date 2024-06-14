0015bda0f755e22a147246717a4256675a14c258
erick <er.rosales@duocuc.cl>
2024-06-14 13:19:51 -0400

    menu listo

---
+++ ejercicio_tipo_prueba.py
@@ -0,0 +1,22 @@
+import os,time
+
+while True:
+    os.system('cls')
+    print("MENU TRABAJODERS")
+    print("1. REGISTRAR TRABAJADOR")
+    print("2. LISTAR TODOS LOS TRABAJADORES")
+    print("3. IMPRIMIR PLANILLA DE SUELDOS")
+    print("4.SALIR")
+    opc=int(input("POR FAVOR ELIJA UNA OPCION: "))
+    os.system('cls')
+
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print ("GRACIAS HASTA LUEGO!!!")
+        break
+    time.sleep(3)
